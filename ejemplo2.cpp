1e35b335514babdc2e781becf68820881e7d4bd4
sandi018u <sandi.gonzalez.ico23@gmail.com>
2019-08-30 12:56:29 -0500

    Es un programa para pedir el nombre la edad y carrera de un estudiante en le que con la ayuda de apuntadores da lo que guardo

--- ejemplo2.cpp
+++ ejemplo2.cpp
@@ -8,11 +8,11 @@ int main(int argc, char** argv) {
 	int mayor =INT_MIN;
 	int *puntero =numeros;
 	for (;puntero<=&numeros[9];puntero++){
-		cout<<"Dame 10 numeros ";
+		cout<<"Dame un numero ";
 		cin>>*puntero;
 		if(*puntero>mayor) mayor=*puntero;
 	}
-	cout<<"El numero mas grande es: "<<mayor;
+	cout<<"El numero mas grande es: "<<mayor<<endl;
 	cout<<"direccion de memoria del arreglo"<<&numeros[10]<<endl;
 	cout<<"direccion del puntero"<<puntero<<endl;
 	
